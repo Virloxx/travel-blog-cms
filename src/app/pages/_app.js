f25e9e4d664b2ec3111ea0f36f495d76ff56382d
jan-c-nowak <jannowak3240@gmail.com>
2024-11-30 20:02:26 +0100

    add plain html from template, css still not working

---
+++ src/app/pages/_app.js
@@ -0,0 +1,7 @@
+import '../../../public/css/main.css'
+import '../../../public/css/noscript.css'
+ 
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
