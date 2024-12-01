6e0c08a90974fb932671d44820013e568cf99e8e
jan-c-nowak <jannowak3240@gmail.com>
2024-12-01 13:46:16 +0100

    add banner component

---
+++ src/app/components/homeBanner/banner.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <section id="banner">
+        <div className="inner">
+            <div className="logo"><span className="icon fa-gem">🤡</span></div>
+            <h2>TO BE REPLACED WITH QUERY TO DB</h2>
+            <p>Banner subtitle + also from db link: <a href="https://en.wikipedia.org/wiki/Better_Call_Saul">SPOILERY</a></p>
+        </div>
+	</section>
+  )
+}
+
+export default Banner
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import SpotlightParent from './components/homeSpotlight/spotlight';
+import Banner from './components/homeBanner/banner';
 
 export default function Home() {
   return (
+    <>
+    <Banner></Banner>
     <SpotlightParent ></SpotlightParent>
+    </>
   );
 }
