a817e3f632482e0303dd686b283c691dc05417fd
Oskar Pankowski <oskar.pankowski01@gmail.com>
2024-12-02 17:10:06 +0100

    add posts section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import SpotlightParent from './components/homeSpotlight/spotlight';
 import Banner from './components/homeBanner/banner';
+import Posts from './components/homePosts/posts';
 
 export default function Home() {
   return (
     <>
     <Banner></Banner>
     <SpotlightParent ></SpotlightParent>
+    <Posts></Posts>
     </>
   );
 }
