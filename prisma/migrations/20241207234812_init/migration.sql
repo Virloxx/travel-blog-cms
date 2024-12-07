36430d16b9bcb0e9b79a00a9b27de2d5b418b52e
jan-c-nowak <84571576+jan-c-nowak@users.noreply.github.com>
2024-12-08 00:52:32 +0100

    add and init sqlite & prisma

---
+++ prisma/migrations/20241207234812_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Spotlight" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "img_url" TEXT NOT NULL
+);
