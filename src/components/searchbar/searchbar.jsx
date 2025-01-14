e7380a782372857571694b74c506399fc23b97ea
Oskar Pankowski <oskar.pankowski01@gmail.com>
2025-01-14 14:25:53 +0100

    add basic search function

--- src/components/searchbar/searchbar.jsx
+++ src/components/searchbar/searchbar.jsx
@@ -1,18 +1,51 @@
 'use client'
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { HiOutlineSearch } from "react-icons/hi";
+import { words } from '../../../lib/data'
 
 const Searchbar = () => {
+
+    const [features, setFeatures] = useState([]);
+    const [activeSearch, setActiveSearch] = useState([])
+
+    useEffect(() => {
+    async function fetchFeatures() {
+        const response = await fetch('/api/post');
+        const data = await response.json();
+        setFeatures(data);
+    }
+    fetchFeatures();
+    }, []);
+
+    const handleSearch = (e) => {
+        if(e.target.value == ''){
+            setActiveSearch([])
+            return false
+        }
+        setActiveSearch(words.filter(w => w.includes(e.target.value)).slice(0,3))
+    }
 
     return (
         <form className='searchbar-container'>
             <div className="searchbar-wrapper">
-                <input type="search" placeholder='Type Here' className='search-input'/>
+                <input type="search" placeholder='Type Here' className='search-input' onChange={(e) => handleSearch(e)}/>
                 <button className='search-button'>
                     <HiOutlineSearch />
                 </button>
-            </div>        
+            </div>
+
+            {
+            activeSearch.length > 0 && (
+                <div className="active-search">
+                    {
+                        activeSearch.map(s => (
+                            <span>{s}</span>
+                        ))
+                    }
+                </div>
+            )
+            } 
         </form>
     );
 };
