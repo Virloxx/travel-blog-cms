a59cc389e12bd6d569db01655d31f66b769602d6
Oskar Pankowski <oskar.pankowski01@gmail.com>
2025-01-22 23:51:33 +0100

    add dashboard

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+function dashboardPage() {
+  return (
+    <div id="page-wrapper">
+        <section id="wrapper">
+            <header>
+                <div className="inner">
+                    <h2>DASHBOARD</h2>
+                </div>
+            </header>
+            <div className="wrapper">
+                <div className="inner">
+                    <section>
+                        <div className="box alt">
+                            <div className="row gtr-uniform">
+                                <div className="col-4"><button className="button primary fit">MANAGE POSTS</button></div>
+                                <div className="col-4"><button className="button primary fit">MANAGE USERS</button></div>
+                                <div className="col-4"><button className="button primary fit">MANAGE SPOTLIGHTS</button></div>
+                                <div className="col-4"><button className="button primary fit">MANAGE FEATURES</button></div>
+                                <div className="col-4"><button className="button primary fit">MANAGE BANNER</button></div>
+                                <div className="col-4"><button className="button primary fit">MANAGE MISC. INFO</button></div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </section>
+    </div>
+  )
+}
+
+export default dashboardPage
