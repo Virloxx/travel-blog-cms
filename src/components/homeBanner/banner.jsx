a60b478875f1656ed33ebdb4bf03d3cf8a83489a
Oskar Pankowski <oskar.pankowski01@gmail.com>
2025-01-25 16:10:12 +0100

    change banner

--- src/components/homeBanner/banner.jsx
+++ src/components/homeBanner/banner.jsx
@@ -4,9 +4,15 @@ function Banner() {
   return (
     <section id="banner">
         <div className="inner">
-            <div className="logo"><span className="icon fa-gem">🤡</span></div>
-            <h2>Travel Blog</h2>
-            <p>Banner subtitle</p>
+            <img
+              className="banner author-photo" 
+              src="https://st2.depositphotos.com/1011382/7489/i/450/depositphotos_74896235-stock-photo-backpacker-man-taking-selfie-on.jpg" 
+              alt="Author photo" 
+            />
+            <div className="banner text-area">
+              <h2>Travel Blog</h2>
+              <p>Banner subtitle</p>
+            </div>
         </div>
 	</section>
   )
