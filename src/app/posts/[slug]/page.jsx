ddd38ff6fcbe3aaa90afa965ca19ac4186d17fa4
Oskar Pankowski <oskar.pankowski01@gmail.com>
2025-01-26 02:17:24 +0100

    fix post page styling

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -5,7 +5,6 @@ import { useEffect, useState } from 'react';
 
 export default function SinglePostPage() {
   const { slug } = useParams();
-
   const [post, setPost] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -25,7 +24,6 @@ export default function SinglePostPage() {
         setLoading(false);
       }
     }
-
     fetchPost();
   }, [slug]);
 
@@ -34,34 +32,28 @@ export default function SinglePostPage() {
   if (!post) return <p>No post found.</p>;
 
   return (
-    <section className="wrapper">
-      <header>
-        <div className="inner">
-          <h2>{post.title}</h2>
-          <p>{post.short_description}</p>
-        </div>
-      </header>
-
-      <div className="wrapper">
-        <div className="inner">
-          <div className="box alt">
-              <div className="row gtr-uniform">
-                <div className="col-12"><span className="image fit"><img src={post.thumbnail_img} alt={post.title} /></span></div>
-                <div className="col-6"><span className="image fit"><img src={post.thumbnail_img} alt={post.title} /></span></div>
-                <div className="col-6"><span className="image fit"><img src={post.thumbnail_img} alt={post.title} /></span></div>
-                {/* <div className="col-4"><span className="image fit"><img src={post.thumbnail_img} alt={post.title} /></span></div> */}
-                <div className="col-4"><span className="image fit"><img src="images/pic07.jpg" alt="" /></span></div>
-                <div className="col-4"><span className="image fit"><img src="images/pic05.jpg" alt="" /></span></div>
-                <div className="col-4"><span className="image fit"><img src="images/pic06.jpg" alt="" /></span></div>
-                <div className="col-4"><span className="image fit"><img src="images/pic06.jpg" alt="" /></span></div>
-                <div className="col-4"><span className="image fit"><img src="images/pic07.jpg" alt="" /></span></div>
-                <div className="col-4"><span className="image fit"><img src="images/pic05.jpg" alt="" /></span></div>
-              </div>
+    <div id="page-wrapper">
+      <section id="wrapper">
+          <header>
+            <div className="inner">
+              <h2>{post.title}</h2>
+              <p>{post.short_description}</p>
             </div>
-          <div dangerouslySetInnerHTML={{ __html: post.content }} />
-          <p><em>Created at: {new Date(post.created_at).toLocaleDateString()}</em></p>
-        </div>
-      </div>
-    </section>
+          </header>
+          <div className="wrapper">
+            <div className="inner">
+              <section>
+                <div className="box alt">
+                    <div className="row gtr-uniform">
+                      <div className="col-11"><span className="image fit"><img src={post.thumbnail_img} alt={post.title} /></span></div>
+                    </div>
+                  </div>
+                <div dangerouslySetInnerHTML={{ __html: post.content }} />
+              </section>
+              <p><em>Created at: {new Date(post.created_at).toLocaleDateString()}</em></p>
+            </div>
+          </div>
+      </section>
+    </div>
   );
 }
