274e7e7ca5b0cc7ac12e08b0346b94765fa5ef7a
jan-c-nowak <84571576+jan-c-nowak@users.noreply.github.com>
2025-03-16 14:16:14 +0100

    fix 2 build errors

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 
-export function Post({ index, title, content, img_url, img_alt }) {
+function Post({ index, title, content, img_url, img_alt }) {
   const router = useRouter();
 
   const handleRouter = () => {
